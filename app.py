bcb32edbce7b9bc0dd9583388cdef66778e02a99
Pronita <22mma007@nith.ac.in>
2024-11-16 01:28:03 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,66 @@
+import streamlit as st
+import pandas as pd
+import preprocessor,helper
+
+df = pd.read_csv('athlete_events.csv')
+region_df = pd.read_csv('noc_regions.csv')
+
+df = preprocessor.preprocess(df,region_df)
+
+st.sidebar.title("Olympics Analysis")
+user_menu = st.sidebar.radio(
+    'Select an Option',
+    ('Medal Tally',"Overall Analysis",'Country-wise Analysis','Athlete wise Analysis')
+)
+
+
+
+if user_menu == 'Medal Tally':
+    st.sidebar.header("Medal Tally")
+    years,country = helper.country_year_list(df)
+    print(years)
+    selected_year = st.sidebar.selectbox("Select Year",years)
+    selected_country = st.sidebar.selectbox("Select Country ",country)
+    medal_tally = helper.fetch_medal_tally(df,selected_year,selected_country)
+
+    if selected_year == 'Overall' and selected_country == 'Overall':
+        st.title("Overall Tally")
+    if selected_year != 'Overall' and selected_country == 'Overall':
+        st.title("Medal Tally in "+ str(selected_year) + "Olympics")
+    if selected_year == 'Overall' and selected_country != 'Overall':
+        st.title(selected_country + " overall performance")
+    if selected_year != 'Overall' and selected_country != 'Overall':
+        st.title(selected_country + " performance in "+ str(selected_year) +" Olympics")
+        
+    st.table(medal_tally)
+
+if user_menu == 'Overall Analysis':
+        editions = df['Year'].unique().shape[0]-1
+        cities = df['City'].unique().shape[0]
+        Sports = df['Sport'].unique().shape[0]
+        Events = df['Event'].unique().shape[0]
+        athletes = df['Name'].unique().shape[0]
+        nations = df['region'].unique().shape[0]
+
+        st.title("Top Statistics")
+        col1,col2,col3 = st.columns(3)
+        with col1:
+            st.header("Editions")
+            st.title(editions)
+        with col2:
+            st.header("Hosts")
+            st.title(cities)
+        with col3:
+            st.header("Sports")
+            st.title(Sports)
+        col1,col2,col3 = st.columns(3)
+        with col1:
+            st.header("Events")
+            st.title(Events)
+        with col2:
+            st.header("Nations")
+            st.title(nations)
+        with col3:
+            st.header("Athletes")
+            st.title(athletes)
+        
